74d21d489a2d94d19c5174eefcf4c8c2206d2f43
oliverLo78 <oliverberto@gmail.com>
2022-11-25 14:57:08 -0500

    changed to force false

--- models/index.js
+++ models/index.js
@@ -1,19 +1,19 @@
 const User = require('./User');
-// const Comment = require('./Comment');
+const Comment = require('./Comment');
 const Post = require('./Post');
 // const { post } = require('../controllers');
 
-// Post.hasMany(Comment, {
-//   foreignKey: 'post_id',
-// });
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
 
-// Comment.belongsTo(User, {
-//   foreignKey: 'user_id',
-// });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
